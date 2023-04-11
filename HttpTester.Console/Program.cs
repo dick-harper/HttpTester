5a0185654bfc8501975ec2af5939e76511c3190a
Richardh Harper <dick.harper@gmail.com>
2023-04-11 05:29:33 -0600

    Create HttpClient factory

--- HttpTester.Console/Program.cs
+++ HttpTester.Console/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http.Headers;
+
+using HttpClient client = new();
+
+client.DefaultRequestHeaders.Accept.Clear();
+
+// accept JSON responses
+client.DefaultRequestHeaders.Accept.Add(
+  new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+
+// necessary to retrieve information from GitHub
+client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+await ProcessRepositoriesAsyncy(client);
+
+static async Task ProcessRepositoriesAsyncy(HttpClient client)
+{
+  var json = await client.GetStringAsync(
+         "https://api.github.com/orgs/dotnet/repos");
+
+  Console.Write(json);
+}
